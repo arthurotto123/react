e711b3cde9bdeac36111a7db41af87d6fbd36069
arthurotto123 <arthurottocoelho@gmail.com>
2025-05-16 11:46:26 -0300

    passos iniciais

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -2,9 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'arthur';
+  const newname = name.toUpperCase();
+
+  function sum(a, b){
+    return a + b;
+  }
+
+  const url = 'https://Via.placeholder.com/150';
+
+  
   return (
     <div className="App">
       <h1>hello word</h1>
+      <p>Olá, {newname}</p>
+      <p>Soma: {sum(1, 2)}</p>
+      <img src={url} alt='minha imagem'/>
     </div>
   );
 }
